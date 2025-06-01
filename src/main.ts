1742872b56ebb62e72338e2d5de19953d1c07241
romanenkoandrew <ramanenkaandrew@gmail.com>
2025-06-01 20:02:05 +0200

    feat: add @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 4000);
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
@@ -16,6 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
